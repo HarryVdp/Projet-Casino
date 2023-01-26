6ca37198f6ff2d41ee11c681d662f17d953da686
vdpharry <vdpharry01@gmail.com>
2023-01-26 18:01:43 +0100

    teste rater

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
@@ -8,31 +7,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Casino</title>
 </head>
-
 <body>
-    <header>
-        <div>
-            <ul>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">#</a>
-                </li>
-                <li>
-                    <a href="#">#</a>
-                </li>
-                <li>
-                    <a href="#">#</a>
-                </li>
-                <div>
-                    <li>
-                        <a href="#">Wallet</a>
-                    </li>
-                </div>
-            </ul>
-        </div>
-    </header>
-</body>
+    <ul>
+        <li>
+            <a href="#">#</a>
+        </li>
+        <li>
+            <a href="#">#</a>
+        </li>
+        <li>
+            <a href="#">#</a>
+        </li>
+        <li>
+            <a href="#">#</a>
+        </li>
+        <li>
+            <a href="#">#</a>
+        </li>
+    </ul>
+   
+        
 
+</body>
 </html>
