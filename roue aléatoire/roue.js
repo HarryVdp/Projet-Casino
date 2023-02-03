946636ecd1c53224dbd795d4f109c5527f8c7944
Van De Perre Harry <vdpharry01@gmail.com>
2023-02-03 10:54:13 +0100

    roue 1chances

--- roue aléatoire/roue.js
+++ roue aléatoire/roue.js
@@ -1,8 +1,13 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
 let number = Math.ceil(Math.random() * 1000);
-
+//let number = 90;
+var angle = 0;
 btn.onclick = function () {
 	container.style.transform = "rotate(" + number + "deg)";
-	number += Math.ceil(Math.random() * 1000);
+	//number += Math.ceil(Math.random() * 1000);
+	console.log(number);
+	angle = number%360;
+	console.log(angle);
+	
 }
--- roue aléatoire/rouealéatoire.php
+++ roue aléatoire/rouealéatoire.php
@@ -3,22 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1,user-scalable=0">
     <link rel="stylesheet" href="styleroue.css">
     <title>Roue de la fortune </title>
 </head>
 <body>
-	<button id="spin">Spin</button>
+	<button id="spin">150<br/>Spin</button>
 	<span class="arrow"></span>
 <div class="container">
-	<div class="one">1</div>
-	<div class="two">2</div>
-	<div class="three">3</div>
-	<div class="four">4</div>
-	<div class="five">5</div>
-	<div class="six">6</div>
-	<div class="seven">7</div>
-	<div class="eight">8</div>
+	
+	<div class="one roue__div"><span>100</span> </div>
+	<div class="two roue__div"><span>200</span> </div>
+	<div class="three roue__div"><span>100</span> </div>
+	<div class="four roue__div"><span>50</span> </div>
+	<div class="five roue__div"><span>400</span> </div>
+	<div class="six roue__div"><span>75</span> </div>
+	<div class="seven roue__div"><span>800</span> </div>
+	<div class="eight roue__div"><span>65</span> </div>
 </div>
     <script src="roue.js"></script>
 </body>
