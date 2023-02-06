1571a5730b31ec7cae5e64149e0c7aeb6c1dd057
Van De Perre Harry <vdpharry01@gmail.com>
2023-02-06 10:50:10 +0100

    avancement 2

--- roue aléatoire/roue.js
+++ roue aléatoire/roue.js
@@ -1,13 +1,15 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 1000);
+let number = Math.ceil(10* Math.random() * 1000);
 //let number = 90;
 var angle = 0;
+container.style.transform = "rotate(" + -11.5 + "deg)";
+number = number - 11.5;
+//number = 360 - 11.5;
 btn.onclick = function () {
 	container.style.transform = "rotate(" + number + "deg)";
 	//number += Math.ceil(Math.random() * 1000);
 	console.log(number);
-	angle = number%360;
+	angle = number%360+11.5;
 	console.log(angle);
-	
 }
