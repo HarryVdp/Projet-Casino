d04c64e631a160f310dbd8b8bce1f394a283ba55
antoinevcndbs <antoine.vandriessche@eleve.cndbs.be>
2023-03-10 10:33:50 +0100

    maj blackjack rassemblement

--- index.php
+++ index.php
@@ -8,52 +8,57 @@ include 'tableauCartes.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
 </head>
 
 <body>
     <h1>Le 21</h1>
-    <?php
-    $random1 = rand(0, 51);
-    $random2 = rand(0, 51);
-    $random3 = rand(0, 51);
-    $random4 = rand(0, 51);
-    $random5 = rand(0, 51);
-    $random6 = rand(0, 51);
-    $random7 = rand(0, 51);
-    $random8 = rand(0, 51);
-    $random9 = rand(0, 51);
-    $random10 = rand(0, 51);
-   
-    echo "<img src='images/", $cartes[$random1]["image"], ".png' alt='Image'>";
-    echo "<img src='images/", $cartes[$random2]["image"], ".png' alt='Image'>";
-    echo "<img src='images/", $cartes[$random3]["image"], ".png' alt='Image'>";
+    <?php  
+    $pioche = $cartes;
+    shuffle($pioche);
 
-    $valeur1 = $cartes[$random1]["valeur"];
-    $valeur2 = $cartes[$random2]["valeur"];
-    $joueur1 = $valeur1 + $valeur2;
-    $valeur3 = $cartes[$random3]["valeur"];
-    $valeur4 = $cartes[$random4]["valeur"];
-    $valeur5 = $cartes[$random5]["valeur"];
-    $valeur6 = $cartes[$random6]["valeur"];
-    $valeur7 = $cartes[$random7]["valeur"];
-    $valeur8 = $cartes[$random8]["valeur"];
-    $valeur9 = $cartes[$random9]["valeur"];
-    $valeur10 = $cartes[$random10]["valeur"];
+    echo "<img src='images/", $pioche[0]["image"], ".png' alt='Image'>";
+    echo "<img src='images/", $pioche[1]["image"], ".png' alt='Image'>";
+    $mainjoueur = [$pioche[0], $pioche[1]];
+    // array splice  permet de supprimer à partir de l'élément en 2e argument si pas de 3e agruent, c'est effacé jusqu'au
+
+    array_splice($pioche,0,2);
+    var_dump($pioche);
+    echo $mainjoueur[0]["valeur"],"<br>", $mainjoueur[1]["valeur"];
 
     ?>
 
-    <form action="button" type="button" name="Add">
-        <input type="button" value="Piocher">
-        <?php $sommecartesjoueur = $joueur1 + $valeur3 ?>
+    <form action="index1.php" method="post">
+        <input type="hidden" name="click" value="piocher" />
+        <button type="submit">Piocher</button>
+    </form>
+    <form action="index1.php" method="post">
+        <input type="hidden" name="click" value="rester" />
+        <button type="submit">Rester</button>
     </form>
 
-    <?php echo $joueur1, "+", $valeur3, "=", $sommecartesjoueur ?>
+    <?php
 
+    function piocher($joueur)
+    {
+        if ($joueur == 'piocher') {
+            $_SESSION['mainJoueur1'] = array_merge($_SESSION['mainJoueur1'], array_splice($_SESSION['pioche'], 0, 1));
+            
+        }
+        if ($joueur == 'rester') {
+            $_SESSION['mainJoueur2'] = array_merge($_SESSION['mainJoueur2'], array_splice($_SESSION['pioche'], 0, 1));
+        }
+    }
 
-
-
-
-
+    if (isset($_POST['click'])) {
+        if ($_POST['click'] == "piocher") {
+            piocher('piocher');
+        }
+        if ($_POST['click'] == "rester") {
+            // piocher('j2');
+        }
+    }
+    ?>
 
 </body>
 
