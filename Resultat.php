6a29962eaf71467fed4278cb9bdeabbeb9387eb9
Nosdogr <jeremy.chaudron@eleve.cndbs.be>
2023-03-20 21:32:13 +0100

    BlackJack M'Bichette

--- Resultat.php
+++ Resultat.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+<link rel="stylesheet" href="style2.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
