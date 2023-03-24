517b295e53ad65d6ecc14acc4e95dafc550030e8
vdpharry <vdpharry01@gmail.com>
2023-03-24 13:50:21 +0100

    Mise en dossier

---
+++ blackjack/index.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+include 'tableauCartes.php';
+?>
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlackJack</title>
+
+</head>
+
+<body>
+    <h1>BlackJack</h1>
+    <?php
+    $_SESSION['pioche'] = $cartes;
+    shuffle($_SESSION['pioche']);
+    // Distribution des 2 cartes au joueur
+    $_SESSION['mainjoueur'] = [$_SESSION['pioche'][0], $_SESSION['pioche'][1]];
+    // array splice  permet de supprimer à partir de l'élément en 2e argument si pas de 3e argument, c'est effacé jusqu'à la fin
+    // array_splice($_SESSION['pioche'],0,2);
+    // var_dump($_SESSION['mainjoueur']);
+    // var_dump($_SESSION['pioche']);
+    echo "<img src='images/", $_SESSION['mainjoueur'][0]["image"], ".png' alt='Image'>";
+    echo "<img src='images/", $_SESSION['mainjoueur'][1]["image"], ".png' alt='Image'>";
+    $_SESSION['sommejoueur'] = $_SESSION['mainjoueur'][0]["valeur"] + $_SESSION['mainjoueur'][1]["valeur"];
+    echo "<br>";
+    echo "La somme de tes cartes est de ", $_SESSION['sommejoueur'];
+    ?>
+    <!-- Si je clique sur le bouton Piocher j'obtiens une variable $_POST['click']=piocher -->
+    <form action="Pioche1Joueur.php" method="post">
+        <input type="hidden" name="click" value="piocher" />
+        <button type="submit">Piocher</button>
+    </form>
+    <form action="Croupier.php" method="post">
+        <input type="hidden" name="click" value="rester" />
+        <button type="submit">Rester</button>
+    </form>
+
+    <?php
+    ?>
+
+</body>
+
+</html>
