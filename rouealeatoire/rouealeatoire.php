b6d8db55c08053f34a4e714044b508b3c14663af
VDPHarry <vdpharry01@gmail.com>
2023-03-31 10:26:30 +0200

    mise en forme page d'accueil

--- rouealeatoire/rouealeatoire.php
+++ rouealeatoire/rouealeatoire.php
@@ -2,6 +2,9 @@
 <html lang="fr">
 <head>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1,user-scalable=0">
